a913acd5b1f7ee31ad47dc04000a09d014e26267
Developer Raj <rajdeepkakar@gmail.com>
2020-10-26 09:54:22 -0400

    Updated the service-worker.js file with a self.addEventListener call back function that runs through the procedure to install a service worker after the service has registered with the browser. The instllation steps include: Open a cache, cache the files, and confirm whether all teh required assets are cached or not.

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,3 +15,29 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 ​
+//Install a service service worker 
+
+// install
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.log("Your files were pre-cached successfully!");
+        cache
+          .addAll(FILES_TO_CACHE)
+          .then((result) => {
+            // debugger;
+            console.log("result of add all", result);
+          })
+          .catch((err) => {
+            // debugger;
+            console.log("Add all error: ", err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  );
+  self.skipWaiting();
+});
