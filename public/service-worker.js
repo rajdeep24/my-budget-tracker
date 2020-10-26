6eb9831d2bfd6b560a11ab9eb9cbce01aa358cfa
Developer Raj <rajdeepkakar@gmail.com>
2020-10-26 09:33:33 -0400

    Created a service-worker.js file and added it to the public folder. In the service worker file, initialized three variables, and assigned them to an array, and two string file names, respectively.

---
+++ public/service-worker.js
@@ -0,0 +1,17 @@
+//Create an array to store list of files that need to be cached into a const variable 
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "db.js",
+    "favicon.ico",
+    "index.js",
+    "manifest.webmanifest",
+    "service-worker.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "styles.css"
+  ];
+​//Initialize two variables with the name 
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+​
