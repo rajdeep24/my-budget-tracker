166cce7e55bfd941a22005774c8f6867994bdd4e
Developer Raj <rajdeepkakar@gmail.com>
2020-10-26 10:10:22 -0400

    Updated the service-worker to include an addEventLister callback function that fetches and returns our cached responses.

--- public/service-worker.js
+++ public/service-worker.js
@@ -59,3 +59,40 @@ self.addEventListener("activate", function (evt) {
   );
   self.clients.claim();
 });
+
+
+// fetch
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+​
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+​
+    return;
+  }
+​
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
