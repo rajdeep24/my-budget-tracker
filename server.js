3e3e14468349f1436a212251ffb48906192112c4
Developer Raj <rajdeepkakar@gmail.com>
2020-10-27 22:34:02 -0400

    Updated connection to mongoose db.

--- server.js
+++ server.js
@@ -15,9 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+//MONGOOSE CONNECTION
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
 });
 
 // routes
